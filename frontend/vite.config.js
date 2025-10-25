235af6a092bc9aea7023c8a360c727d14c44feb0
kaleeswaran18 <kaleesvk1997@gmail.com>
2025-10-25 14:33:33 +0530

    check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BetterThis-/', // <-- set this to your repo name
+  base: "./",       // relative paths to index.html
+  build: {
+    outDir: 'docs', // directly build into docs folder
+    assetsDir: 'assets'
+  }
 })
