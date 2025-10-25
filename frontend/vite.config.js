df683c1c6798fc3c4714967d587f2d571ab04494
kaleeswaran18 <kaleesvk1997@gmail.com>
2025-10-25 14:26:41 +0530

    check

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./"   // <-- add this line
+  base: "./",             // relative paths for GitHub Pages
+  build: {
+    outDir: 'docs',       // directly build into docs folder
+    assetsDir: 'assets',  // default, can keep as 'assets'
+  }
 })
