b6049451c2f1e6627d2949899816c3088af53bd1
kaleeswaran18 <kaleesvk1997@gmail.com>
2025-10-25 14:17:39 +0530

    push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BetterThis-/', // <-- set this to your repo name
+  base: "./"   // <-- add this line
 })
